b6136195a677576014f361a5c5d5ec5ad5c8c673
YAPI <yapi@Kamil-imac.localdomain>
2019-01-02 17:00:48 -0800

    closer

--- express_react_example/webpack.conf.js
+++ express_react_example/webpack.conf.js
@@ -1,28 +1,25 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
+const appDIR = path.join(__dirname,'client/my-app/source/index.js');
+console.log(typeof appDIR);
+
+
 
 module.exports = {
-    entry: 'client/my-app/src/index.js',
+    entry: appDIR,
     output: {
-        filename: 'build.js',
-        path: path.join(__dirname,'/client/my-app/dist')
-    },
-    module : {
-        rules : [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {loader:"babel-loader"}
+                filename: 'build.js',
+                path: path.join(__dirname,'/client/my-app/dist')
             },
-            {
-                test: /\.html$/,
-                use: [
-                  {
-                    loader: "html-loader",
-                    options: { minimize: true }
-                  }
-                ]
-              }
-        ]
-    }
+    module : {
+            rules : [
+                        {
+                            test: /\.js$/,
+                            exclude: /node_modules/,
+                            use: {loader:"babel-loader"}
+                        }
+                       
+                    ]
+        },
+    plugins : [new HWP()]    
 }
