6c8bac9bfbc99464c0ca1ddb1925f297cf44c33a
YAPI <yapi@Kamil-imac.localdomain>
2019-01-03 16:15:07 -0800

    webpack works

--- express_react_example/webpack.conf.js
+++ express_react_example/webpack.conf.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
 const appDIR = path.join(__dirname,'client/my-app/source/index.js');
+// import css from 'file.css';
 console.log(typeof appDIR);
 
 
@@ -14,12 +15,23 @@ module.exports = {
     module : {
             rules : [
                         {
-                            test: /\.js$/,
+                            test: /\.js|jsx/,
                             exclude: /node_modules/,
-                            use: {loader:"babel-loader"}
+                            use: {
+                                loader:"babel-loader"
+                            }
+                        },
+                        {
+                            test:/\.css$/,
+                            exclude: /node_modules/,
+                            use: ["style-loader",'css-loader']
+                        },
+                        {
+                            test: /\.svg$/,
+                            loader: 'svg-inline-loader'
                         }
                        
                     ]
-        },
-    plugins : [new HWP()]    
+        }
+       
 }
